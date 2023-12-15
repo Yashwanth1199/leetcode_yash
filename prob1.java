28b883a58586c2423b8c4eb83605d3ff5d3d0b07
Yashwanth <57251033+Yashwanth1199@users.noreply.github.com>
2023-12-15 22:02:29 +0530

    Add files via upload

---
+++ prob1.java
@@ -0,0 +1,26 @@
+//Sort Array by Parity
+//Even nums first followed by odd nums
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int e=0;//intializing a var to 0 to replace it with odd elements using it as an index
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                //basic swapping technique using third var
+                int temp=nums[i];
+                nums[i]=nums[e];
+                nums[e++]=temp;//incrementing e everytime the control comes in here
+            }
+        }
+        return nums;
+    }
+}
+class Demo{
+    public static void main(String[] args) {
+        int nums[]={3,1,2,4};
+        Solution s= new Solution();
+        int n[]=s.sortArrayByParity(nums);
+        for(int p:n){
+            System.out.print(p+" ");
+        }
+    }
+}
